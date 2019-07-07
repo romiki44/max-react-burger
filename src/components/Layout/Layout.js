ee631ce10986a70d910ac060864aed8533aa53b3
Roman Kiss <romiki44@hotmail.com>
2019-07-07 07:43:30 +0200

    creating layout component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Aux from '../../hoc/Auxiliary';
+
+const Layout = (props) => (
+  <Aux>
+    <div>Toolbar, SideBar, BackDrop</div>
+    <main>
+      {props.children}
+    </main>
+  </Aux>
+);
+
+export default Layout;
