1b40b64135c4a574ce031c52b064d147063fc612
Roman Kiss <romiki44@hotmail.com>
2019-07-07 10:04:42 +0200

    claculating the ingredients sum automaticly

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.module.css';
 
 const Layout = (props) => (
   <Aux>
     <div>Toolbar, SideBar, BackDrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
