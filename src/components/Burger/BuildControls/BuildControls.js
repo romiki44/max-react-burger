8d1f70a2e9c5a5119cbe302b77977574245489ce
Roman Kiss <romiki44@hotmail.com>
2019-08-05 14:27:45 +0200

    section18 done

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -26,7 +26,7 @@ const BuildControls = (props) => {
           className={classes.OrderButton}
           disabled={!props.purchasable}
           onClick={props.ordered}>
-          ORDER NOW
+            {props.isAuth ? 'ORDER NOW' : 'SIGN UP TO ORDER'}
         </button>
       </div>
     )
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -19,7 +19,7 @@ const SideDrawer = (props) => {
           <Logo />
         </div>
         <nav>
-          <NavigationItems></NavigationItems>
+          <NavigationItems isAuthenticated={props.isAuth} />
         </nav>
       </div>
     </Aux>
